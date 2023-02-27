57a51b4ee6f3cc4c1ae25f93ce15e47224d3c605
nnecec <nnecec@outlook.com>
2023-02-27 11:50:17 +0800

    feat(docs): test build

--- apps/docs/demos/remember/index.tsx
+++ apps/docs/demos/remember/index.tsx
@@ -1,13 +1,13 @@
 import {
-  SandpackProvider,
   SandpackCodeEditor,
-  SandpackLayout,
   SandpackConsole,
+  SandpackLayout,
+  SandpackProvider,
 } from '@codesandbox/sandpack-react'
 
 export const RememberExample = () => {
   const files = {
-    '/index.js': `console.log('Hello world!')`
+    '/index.js': 'console.log(\'Hello world!\')',
   }
   return (
     <SandpackProvider files={files} theme="auto" template="vanilla">
